ee0f56c3c7e69e02f5add9d66f192d69f79245f1
Ziga <zigaresnik@gmail.com>
2016-01-02 16:47:12 +0100

    basic structure

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+# Import Flask and Flask Restful
+from flask import Flask
+from flask_restful import Api
+
+# Import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Define the uWSGI application object
+app = Flask(__name__)
+
+# Define main entry point for the application
+api = Api(app)
+
+
+# Configurations
+app.config.from_object('config')
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
---
+++ config.py
@@ -0,0 +1,10 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database - we are working with
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'mFRI.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
